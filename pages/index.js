b5a989eed8c4e8d640f57de8dea337f84db9b04a
mfrickss <ricardocamargodev@gmail.com>
2026-03-31 17:48:24 -0300

    feat: add /api/v1/status endpoint

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,4 @@ function Home() {
   return <h1>Hello World!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indention wrong nessa porra");
-}
-teste();
-
 export default Home;
