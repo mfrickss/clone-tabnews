a89dba05879c72954f0f63db713a36c1943f6310
mfrickss <ricardocamargodev@gmail.com>
2026-04-22 17:52:29 -0300

    att resumo

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,7 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ Alunos: "São acima da média" });
+  response.status(200).json({});
 }
 
 export default status;
